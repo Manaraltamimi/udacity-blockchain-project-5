3005bd20097fa39bbb29fce3c3b2c4edc04638cb
Thomas Weibel <thom@ikhono.net>
2019-02-07 14:18:32 +0100

    Add test to check if exchangeStars works correctly

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -9,10 +9,10 @@ contract('StarNotary', (accs) => {
 });
 
 it('can Create a Star', async() => {
-    let tokenId = 1;
     let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]})
-    assert.equal(await instance.tokenIdToStarInfo.call(tokenId), 'Awesome Star!')
+    let starId = 1;
+    await instance.createStar('Awesome Star!', starId, {from: accounts[0]})
+    assert.equal(await instance.tokenIdToStarInfo.call(starId), 'Awesome Star!')
 });
 
 it('lets user1 put up their star for sale', async() => {
@@ -76,9 +76,10 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
 // Implement Task 2 Add supporting unit tests
 
 it('can add the star name and star symbol properly', async() => {
+    let instance = await StarNotary.deployed();
+
     // 1. create a Star with different tokenId
     let tokenId = 6;
-    let instance = await StarNotary.deployed();
     await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]}); // not really necessary as name and symbol are constant state variables
 
     // 2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
@@ -87,9 +88,23 @@ it('can add the star name and star symbol properly', async() => {
 });
 
 it('lets 2 users exchange stars', async() => {
+    let instance = await StarNotary.deployed();
+
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+
     // 1. create 2 Stars with different tokenId
+    let starId1 = 7;
+    let starId2 = 8;
+    await instance.createStar('Star 1', starId1, {from: user1});
+    await instance.createStar('Star 2', starId2, {from: user2});
+
     // 2. Call the exchangeStars functions implemented in the Smart Contract
+    await instance.exchangeStars(starId1,starId2, {from: user1});
+
     // 3. Verify that the owners changed
+    assert.equal(await instance.ownerOf(starId1), user2);
+    assert.equal(await instance.ownerOf(starId2), user1);
 });
 
 it('lets a user transfer a star', async() => {
@@ -99,10 +114,11 @@ it('lets a user transfer a star', async() => {
 });
 
 it('lookupTokenIdToStarInfo test', async() => {
-    // 1. create a Star with different tokenId
-    let starId = 9;
     let instance = await StarNotary.deployed();
-    await instance.createStar('lookupTokenIdToStarInfo test', starId, {from: accounts[0]}); // not really necessary as name and symbol are constant state variables
+
+    // 1. create a Star with different tokenId
+    let starId = 10;
+    await instance.createStar('lookupTokenIdToStarInfo test', starId, {from: accounts[0]});
 
     // 2. Call your method lookupTokenIdToStarInfo
     // 3. Verify if you Star name is the same
